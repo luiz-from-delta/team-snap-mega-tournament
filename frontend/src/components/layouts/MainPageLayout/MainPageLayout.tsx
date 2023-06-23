c71f6630bc6a064e5e98faa22755563126efeb28
Luiz Antônio Neto <luizneto.ifnmg@gmail.com>
2023-06-23 17:56:21 -0300

    feat(layout): create page layout component

---
+++ frontend/src/components/layouts/MainPageLayout/MainPageLayout.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import { Header } from "../../atoms/Header";
+
+import { MainPageLayoutProps } from "./types";
+import { TournamentTitle } from "../../atoms/TournamentTitle";
+import { Button } from "../../atoms/Button";
+import { Plus, UsersFour } from "phosphor-react";
+import { FlexBox } from "../../atoms/FlexBox";
+import { Breadcrumb } from "../../atoms/Breadcrumb";
+import { Link } from "react-router-dom";
+
+export const MainPageLayout: React.FC<MainPageLayoutProps> = ({
+  breadcrumbProps,
+  children,
+}) => {
+  return (
+    <main>
+      <Header />
+      <FlexBox
+        as="section"
+        direction="column"
+        container={{
+          isCentered: true,
+          maxWidth: 1200,
+        }}
+      >
+        <FlexBox
+          justify="between"
+          align="center"
+          width="full"
+          style={{ marginBottom: 85 }}
+        >
+          <FlexBox direction="column" gap="1rem">
+            <TournamentTitle />
+            <Breadcrumb {...breadcrumbProps} />
+          </FlexBox>
+          <FlexBox gap="1rem">
+            <Link to="/teams">
+              <Button icon={<UsersFour size={18} />}>View Teams</Button>
+            </Link>
+            <Link to="/join">
+              <Button icon={<Plus size={18} />}>Register Teams</Button>
+            </Link>
+          </FlexBox>
+        </FlexBox>
+        {children}
+      </FlexBox>
+    </main>
+  );
+};
---
+++ frontend/src/components/layouts/MainPageLayout/types.ts
@@ -0,0 +1,6 @@
+import { BreadcrumbProps } from "../../atoms/Breadcrumb/types";
+
+export type MainPageLayoutProps = {
+  breadcrumbProps: BreadcrumbProps;
+  children: React.ReactNode;
+};
